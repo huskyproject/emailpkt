1f427516f127fda82fe8d14ef2530a535ca3e09b
Stas Degteff <stas_degteff@users.sourceforge.net>
2002-10-28 10:30:40 +0000

    Add/update license information and add/change some doc

--- h/ecommon.h
+++ h/ecommon.h
@@ -1,10 +1,31 @@
 /* Emailpkt: Husky e-mailer
  *
  * (C) Copyleft Stas Degteff 2:5080/102@FIDOnet, g@grumbler.org
+ * (c) Copyleft Husky developers team http://husky.sorceforge.net/team.html
  *
  * $Id$
  * Common declarations
  *
+ * For latest version see http://husky.sorceforge.net/emailpkt
+ *
+ * This file is part of EMAILPKT, module of The HUSKY Fidonet Software.
+ *
+ * EMAILPKT is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * EMAILPKT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; see file COPYING. If not, write to the Free
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * See also http://www.gnu.org, license may be found here.
+ *****************************************************************************
  */
 
 #ifndef __ECOMMON_H
